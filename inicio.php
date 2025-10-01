c14a15c83a93cb276830215f83ec397b5296d9cf
Dante <dante.160110@gmail.com>
2025-10-01 17:03:28 -0300

    Add files via upload

--- inicio.php
+++ inicio.php
@@ -47,23 +47,6 @@
             .logout-link:hover {
                 text-decoration: underline;
             }
-            .conteiner-pai {
-                display: flex;
-                justify-content: center; 
-                align-items: flex-start;
-                gap: 50px; 
-                }
-            .conteiner-filho {
-                display: center;
-                justify-content: center; 
-                align-items: flex-start; 
-            }  
-            .graf {
-                 display: flex;
-                justify-content: center;
-                align-items: flex-start;
-                gap: 50px; 
-            }
         </style>
     </head>
     <body>
@@ -85,96 +68,94 @@
             ?>
         </nav>
         <br>
-        <center><h2><svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" fill="currentColor" class="bi bi-house-fill" viewBox="0 0 16 16">
-            <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L8 2.207l6.646 6.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293z"/>
-            <path d="m8 3.293 6 6V13.5a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 13.5V9.293z"/>
-            </svg>&nbsp;DASHBOARD</h2></center>
-        <br>
-        <div class="conteiner-pai">
-            <div class="conteiner-filho row justify-content row-cols-3 row-cols-md-1 mb-2 text-center">                                    
-                <div class="col">
-                    <div class="card mb-4 rounded-3 shadow-sw">
-                        <div  class="card-header py-3">
-                            <h2>TOTAL DE CLIENTES</h2>
-                </div>
-                                <table class="table table-hover">
-                                    <tr>
-                                        <?php
-                                            $pesquisa1 = mysqli_query($conn, "SELECT COUNT(id) as numcli FROM `clientes`;");
-                                                $row = mysqli_num_rows($pesquisa1);
-                                                if ($row > 0) {
-                                                while ($registro = $pesquisa1 -> fetch_array()) {
-                                                $numcli = $registro ['numcli'];
-                                                echo ($numcli);
-                                                    }
-                                                }
-                                        ?>
-                                    </tr>
-                                </table>
-                        </div>
-                </div>
+        <center><h2>DASHBOARD</h2></center>
+        <br/>
+        <div class="row justify-content-center row-cols-1 row-cols-md-4 mb-4 text-center">
+            <div class="col">
+                <div class="card mb-4 rounded-3 shadow-sw">
+                    <div class="card-header py-3">
+                        <h2>CLIENTES</h2>
+                    </div>
+                    <div class="card-body text-center" style="background-color: #e3f2fd;">
+                        <center>
+                        <?php
+                            $sql = "SELECT count(id) AS numcli FROM clientes";
+                            $resultado = mysqli_query($conn, $sql);
+                            $dados = mysqli_fetch_assoc($resultado);
+                            $total_clientes = $dados['numcli'];
+                        ?>
+                        <table>
+                            <tr>
+                                <td><svg xmlns="http://www.w3.org/2000/svg" width="55" height="55" fill="currentColor" class="bi bi-people" viewBox="0 0 16 16">
+                                    <path d="M15 14s1 0 1-1-1-4-5-4-5 3-5 4 1 1 1 1zm-7.978-1L7 12.996c.001-.264.167-1.03.76-1.72C8.312 10.629 9.282 10 11 10c1.717 0 2.687.63 3.24 1.276.593.69.758 1.457.76 1.72l-.008.002-.014.002zM11 7a2 2 0 1 0 0-4 2 2 0 0 0 0 4m3-2a3 3 0 1 1-6 0 3 3 0 0 1 6 0M6.936 9.28a6 6 0 0 0-1.23-.247A7 7 0 0 0 5 9c-4 0-5 3-5 4q0 1 1 1h4.216A2.24 2.24 0 0 1 5 13c0-1.01.377-2.042 1.09-2.904.243-.294.526-.569.846-.816M4.92 10A5.5 5.5 0 0 0 4 13H1c0-.26.164-1.03.76-1.724.545-.636 1.492-1.256 3.16-1.275ZM1.5 5.5a3 3 0 1 1 6 0 3 3 0 0 1-6 0m3-2a2 2 0 1 0 0 4 2 2 0 0 0 0-4"/>
+                                </svg></td>
+                                <td>&nbsp;&nbsp;<h4>TOTAL</h4></td>
+                            </tr>
+                            <tr>
+                                <td colspan="2"><center><?php echo "<font size='40'><b>".$total_clientes."</b></font>"; ?></center></td>
+                            </tr>
+                        </table>
+                        </center>
+                    </div>
+                </div>       
             </div>
-            <div class="conteiner-filho row justify-content row-cols-3 row-cols-md-1 mb-2 text-center">                                    
-                <div class="col">
-                    <div class="card mb-4 rounded-3 shadow-sw">
-                        <div  class="card-header py-3">
-                        <h2>TOTAL DE SALGADOS</h2>
+            <div class="col">
+                <div class="card mb-4 rounded-3 shadow-sw">
+                    <div class="card-header py-3">
+                        <h2>SALGADOS</h2>
                     </div>
-                            <table class="table table-hover">
-                                <tr>
-					                <?php
-                                        $pesquisa2 = mysqli_query($conn, "SELECT COUNT(id) as numsal FROM `salgados`;");
-                                            $row = mysqli_num_rows($pesquisa2);
-                                            if ($row > 0) {
-                                            while ($registro = $pesquisa2 -> fetch_array()) {
-                                            $numsal = $registro ['numsal'];
-                                            echo ($numsal);
-                                                }
-                                            }
-					                ?>
-                                </tr>
-                            </table>
+                    <div class="card-body text-start" style="background-color: #7FFFD4;">
+                        <center>
+                        <?php
+                            $sql = "SELECT count(id) AS numsal FROM salgados";
+                            $resultado = mysqli_query($conn, $sql);
+                            $dados = mysqli_fetch_assoc($resultado);
+                            $total_salgados = $dados['numsal'];
+                        ?>
+                        <table>
+                            <tr>
+                                <td><svg xmlns="http://www.w3.org/2000/svg" width="55" height="55" fill="currentColor" class="bi bi-rocket-takeoff" viewBox="0 0 16 16">
+                                <path d="M9.752 6.193c.599.6 1.73.437 2.528-.362s.96-1.932.362-2.531c-.599-.6-1.73-.438-2.528.361-.798.8-.96 1.933-.362 2.532"/>
+                                <path d="M15.811 3.312c-.363 1.534-1.334 3.626-3.64 6.218l-.24 2.408a2.56 2.56 0 0 1-.732 1.526L8.817 15.85a.51.51 0 0 1-.867-.434l.27-1.899c.04-.28-.013-.593-.131-.956a9 9 0 0 0-.249-.657l-.082-.202c-.815-.197-1.578-.662-2.191-1.277-.614-.615-1.079-1.379-1.275-2.195l-.203-.083a10 10 0 0 0-.655-.248c-.363-.119-.675-.172-.955-.132l-1.896.27A.51.51 0 0 1 .15 7.17l2.382-2.386c.41-.41.947-.67 1.524-.734h.006l2.4-.238C9.005 1.55 11.087.582 12.623.208c.89-.217 1.59-.232 2.08-.188.244.023.435.06.57.093q.1.026.16.045c.184.06.279.13.351.295l.029.073a3.5 3.5 0 0 1 .157.721c.055.485.051 1.178-.159 2.065m-4.828 7.475.04-.04-.107 1.081a1.54 1.54 0 0 1-.44.913l-1.298 1.3.054-.38c.072-.506-.034-.993-.172-1.418a9 9 0 0 0-.164-.45c.738-.065 1.462-.38 2.087-1.006M5.205 5c-.625.626-.94 1.351-1.004 2.09a9 9 0 0 0-.45-.164c-.424-.138-.91-.244-1.416-.172l-.38.054 1.3-1.3c.245-.246.566-.401.91-.44l1.08-.107zm9.406-3.961c-.38-.034-.967-.027-1.746.163-1.558.38-3.917 1.496-6.937 4.521-.62.62-.799 1.34-.687 2.051.107.676.483 1.362 1.048 1.928.564.565 1.25.941 1.924 1.049.71.112 1.429-.067 2.048-.688 3.079-3.083 4.192-5.444 4.556-6.987.183-.771.18-1.345.138-1.713a3 3 0 0 0-.045-.283 3 3 0 0 0-.3-.041Z"/>
+                                <path d="M7.009 12.139a7.6 7.6 0 0 1-1.804-1.352A7.6 7.6 0 0 1 3.794 8.86c-1.102.992-1.965 5.054-1.839 5.18.125.126 3.936-.896 5.054-1.902Z"/>
+                                </svg></td>
+                                <td>&nbsp;&nbsp;<h4>TOTAL</h4></td>
+                            </tr>
+                            <tr>
+                                <td colspan="2"><center><?php echo "<font size='40'><b>".$total_salgados."</b></font>"; ?></center></td>
+                            </tr>
+                        </table>
+                        </center>
                     </div>
-                </div>
+                </div>       
             </div>
-            <div class="conteiner-filho row justify-content row-cols-3 row-cols-md-1 mb-2 text-center">                                    
-                <div class="col">
-                    <div class="card mb-4 rounded-3 shadow-sw">
-                        <div  class="card-header py-3">
-                        <h2>TOTAL DE PEDIDOS</h2>
+            <div class="col">
+                <div class="card mb-4 rounded-3 shadow-sw">
+                    <div class="card-header py-3">
+                        <h2>PEDIDOS</h2>
                     </div>
-                            <table class="table table-hover">
-                                <tr>
-					                <?php
-     					                $pesquisa3 = mysqli_query($conn, "SELECT COUNT(id) as numped FROM `pedidos`;");
-                                            $row = mysqli_num_rows($pesquisa3);
-                                            if ($row > 0) {
-                                            while ($registro = $pesquisa3 -> fetch_array()) {
-                                            $numped = $registro ['numped'];
-                                            echo ("Total de Pedidos".$numped);
-                                                }
-                                            }
-                                            ?>
-                                            <br>
-                                            <?php
-                                            $pesquisa4 = mysqli_query($conn, "SELECT SUM(total) AS numtotal FROM pedidos;");
-                                            $row = mysqli_num_rows($pesquisa4);
-                                            if ($row > 0) {
-                                            while ($registro = $pesquisa4 -> fetch_array()) {
-                                            $numtotal = $registro ['numtotal'];
-                                            echo ("Valor em pedidos R$".$numtotal);
-                                                }
-                                            }
-                                    ?>
-                                </tr>
-                            </table>
-                        </div>
-                </div>
+                    <div class="card-body text-start" style="background-color: #FFDAB9;">
+                        <?php
+                            $sql = "SELECT count(id) AS numped FROM pedidos";
+                            $resultado = mysqli_query($conn, $sql);
+                            $dados = mysqli_fetch_assoc($resultado);
+                            $total_pedidos = $dados['numped'];
+                            $sql2 = "select sum(total) as total from pedidos";
+                            $resultado2 = mysqli_query($conn, $sql2);
+                            $dados2 = mysqli_fetch_assoc($resultado2);
+                            $total_dindin = $dados2['total'];
+                            $total = number_format($total_dindin, 2, ',', '.');
+                        ?>
+                        <?php echo "TOTAL DE PEDIDOS: <font size='6'><b>".$total_pedidos."</b></font>"; ?>
+                        <p><?php echo "TOTAL(R$): <font size='7'><b>".$total."</b></font>"; ?></p>
+                    </div>
+                </div>       
             </div>
         </div>
-        <div class="graf row-cols-md-3 text-center">
+        <br/>
+        <div class="row justify-content-center row-cols-1 row-cols-md-4 mb-4 text-center">
             <div class="col">
-                <div class="card mb- rounded-5  shadow-sw">
+                <div class="card mb-4 rounded-3 shadow-sw">
                     <div class="card-header py-3">
                         <h2>PEDIDOS POR CLIENTES</h2>
                     </div>
@@ -185,6 +166,30 @@
                     </div>
                 </div>       
             </div>
-        </div>		                                                           
+            <div class="col">
+                <div class="card mb-4 rounded-3 shadow-sw">
+                    <div class="card-header py-3">
+                        <h2>PEDIDOS POR SALGADOS</h2>
+                    </div>
+                    <div class="card-body text-start">
+                        <?php
+                            include 'grafpedsal.php';
+                        ?>
+                    </div>
+                </div>       
+            </div>
+            <div class="col">
+                <div class="card mb-4 rounded-3 shadow-sw">
+                    <div class="card-header py-3">
+                        <h2>PEDIDOS POR CIDADES</h2>
+                    </div>
+                    <div class="card-body text-start">
+                        <?php
+                            include 'grafpedcid.php';
+                        ?>
+                    </div>
+                </div>       
+            </div>
+        </div>
     </body>
 </html>
